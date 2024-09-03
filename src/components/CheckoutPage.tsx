0d4243637058adf7984d28749d234e0c4fd93e1f
DanZ-Z <zuladaniel131@gmail.com>
2024-09-03 11:01:12 +0200

    Update

--- src/components/CheckoutPage.tsx
+++ src/components/CheckoutPage.tsx
@@ -49,7 +49,7 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
       elements,
       clientSecret,
       confirmParams: {
-        return_url: `http://www.localhost:3000/payment-success?amount=${amount}`,
+        return_url: `https://caseshark.vercel.app//payment-success?amount=${amount}`,
       },
     });
 
